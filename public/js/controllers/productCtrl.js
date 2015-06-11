3a284489509a8bbc457b05e60151d652c604fa76
chardyy <chardy.orofeo@yahoo.com.ph>
2015-06-11 11:53:10 +0800

    done querying data from database.

--- public/js/controllers/productCtrl.js
+++ public/js/controllers/productCtrl.js
@@ -3,6 +3,7 @@ var product = angular.module('ProductCtrl', []);
 product.controller('ProductsController', function($scope, CRUD){
 
 	var getProducts = CRUD.all();
+	
 		getProducts.success(function(data){
 			$scope.products = data;
 		});
